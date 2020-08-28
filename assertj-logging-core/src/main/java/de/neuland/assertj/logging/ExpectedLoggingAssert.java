b822837af1f00208d7af9449a9f87963509caf52
Christian Stuht <christian.stuht@neuland-bfi.de>
2020-08-28 08:28:34 +0200

    add hasNo...Message assertions

--- assertj-logging-core/src/main/java/de/neuland/assertj/logging/ExpectedLoggingAssert.java
+++ assertj-logging-core/src/main/java/de/neuland/assertj/logging/ExpectedLoggingAssert.java
@@ -3,6 +3,10 @@ package de.neuland.assertj.logging;
 import org.assertj.core.api.Condition;
 import org.assertj.core.api.ListAssert;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static de.neuland.assertj.logging.LogLevel.*;
 import static org.assertj.core.api.Assertions.tuple;
 
 
@@ -19,43 +23,67 @@ public class ExpectedLoggingAssert extends ListAssert<LogEvent> {
     }
 
     public ExpectedLoggingAssert hasErrorMessage(String actualMessage) {
-        return hasMessageWithLevel(LogLevel.ERROR, actualMessage);
+        return hasMessageWithLevel(ERROR, actualMessage);
     }
 
     public ExpectedLoggingAssert hasErrorMessage(String message, Throwable throwable) {
-        return hasMessageWithLevelAndThrowable(LogLevel.ERROR, message, throwable);
+        return hasMessageWithLevelAndThrowable(ERROR, message, throwable);
     }
 
     public ExpectedLoggingAssert hasErrorMessageMatching(String regex) {
-        return hasMessageMatching(LogLevel.ERROR, regex);
+        return hasMessageMatching(ERROR, regex);
     }
 
     public ExpectedLoggingAssert hasErrorMessageMatching(String regex, Throwable throwable) {
-        return hasMessageMatching(LogLevel.ERROR, regex, throwable);
+        return hasMessageMatching(ERROR, regex, throwable);
+    }
+
+    public ExpectedLoggingAssert hasNoErrorMessage() {
+        return hasNoMessageWithLevel(errorMessages(), ERROR);
+    }
+
+    public ListAssert<LogEvent> errorMessages() {
+        return messagesWithLevel(ERROR);
     }
 
     public ExpectedLoggingAssert hasWarningMessage(String actualMessage) {
-        return hasMessageWithLevel(LogLevel.WARNING, actualMessage);
+        return hasMessageWithLevel(WARNING, actualMessage);
     }
 
     public ExpectedLoggingAssert hasWarningMessage(String actualMessage, Throwable actualDetail) {
-        return hasMessageWithLevelAndThrowable(LogLevel.WARNING, actualMessage, actualDetail);
+        return hasMessageWithLevelAndThrowable(WARNING, actualMessage, actualDetail);
     }
 
     public ExpectedLoggingAssert hasWarningMessageMatching(String regex) {
-        return hasMessageMatching(LogLevel.WARNING, regex);
+        return hasMessageMatching(WARNING, regex);
     }
 
     public ExpectedLoggingAssert hasWarningMessageMatching(String regex, Throwable throwable) {
-        return hasMessageMatching(LogLevel.WARNING, regex, throwable);
+        return hasMessageMatching(WARNING, regex, throwable);
+    }
+
+    public ExpectedLoggingAssert hasNoWarningMessage() {
+        return hasNoMessageWithLevel(warningMessages(), WARNING);
+    }
+
+    public ListAssert<LogEvent> warningMessages() {
+        return messagesWithLevel(WARNING);
     }
 
     public ExpectedLoggingAssert hasInfoMessage(String actualMessage) {
-        return hasMessageWithLevel(LogLevel.INFO, actualMessage);
+        return hasMessageWithLevel(INFO, actualMessage);
     }
 
     public ExpectedLoggingAssert hasInfoMessageMatching(String regex) {
-        return hasMessageMatching(LogLevel.INFO, regex);
+        return hasMessageMatching(INFO, regex);
+    }
+
+    public ListAssert<LogEvent> hasNoInfoMessage() {
+        return hasNoMessageWithLevel(infoMessages(), INFO);
+    }
+
+    public ListAssert<LogEvent> infoMessages() {
+        return messagesWithLevel(INFO);
     }
 
     private ExpectedLoggingAssert hasMessageMatching(final LogLevel logLevel, final String regex) {
@@ -63,44 +91,32 @@ public class ExpectedLoggingAssert extends ListAssert<LogEvent> {
                         logLevel,
                         regex,
                         joinLogEvents())
-                .haveAtLeastOne(new Condition<LogEvent>() {
-                    @Override public boolean matches(LogEvent logEvent) {
-                        return logEvent.getLevel() == logLevel &&
-                               logEvent.matchesMessage(regex);
-                    }
-                });
+                        .haveAtLeastOne(new Condition<LogEvent>() {
+                            @Override public boolean matches(LogEvent logEvent) {
+                                return logEvent.getLevel() == logLevel &&
+                                       logEvent.matchesMessage(regex);
+                            }
+                        });
 
         return this;
     }
 
-    public ExpectedLoggingAssert hasMessageMatching(final LogLevel logLevel,
+    private ExpectedLoggingAssert hasMessageMatching(final LogLevel logLevel,
                                                     final String regex,
                                                     final Throwable throwable) {
         withFailMessage("Expected %s message matching '%s'.\nBut only found:\n%s",
                         logLevel,
                         regex,
                         joinLogEvents())
-                .haveAtLeastOne(new Condition<LogEvent>() {
-                    @Override public boolean matches(LogEvent logEvent) {
-                        return logEvent.getLevel() == logLevel &&
-                               logEvent.matchesMessage(regex) &&
-                               logEvent.getThrowable() == throwable;
-                    }
-                });
+                        .haveAtLeastOne(new Condition<LogEvent>() {
+                            @Override public boolean matches(LogEvent logEvent) {
+                                return logEvent.getLevel() == logLevel &&
+                                       logEvent.matchesMessage(regex) &&
+                                       logEvent.getThrowable() == throwable;
+                            }
+                        });
 
         return this;
-    }
-
-    public ListAssert<LogEvent> errorMessages() {
-        return messagesWithLevel(LogLevel.ERROR);
-    }
-
-    public ListAssert<LogEvent> warningMessages() {
-        return messagesWithLevel(LogLevel.WARNING);
-    }
-
-    public ListAssert<LogEvent> infoMessages() {
-        return messagesWithLevel(LogLevel.INFO);
     }
 
     private ListAssert<LogEvent> messagesWithLevel(LogLevel logLevel) {
@@ -111,19 +127,45 @@ public class ExpectedLoggingAssert extends ListAssert<LogEvent> {
                                                                   String actualMessage,
                                                                   Throwable actualThrowable) {
         extracting("message", "level", "throwable")
-                .contains(tuple(actualMessage, logLevel, actualThrowable));
+                        .contains(tuple(actualMessage, logLevel, actualThrowable));
 
         return this;
     }
 
     private ExpectedLoggingAssert hasMessageWithLevel(LogLevel logLevel, String actualMessage) {
         extracting("level", "message")
-                .contains(tuple(logLevel, actualMessage));
+                        .contains(tuple(logLevel, actualMessage));
 
         return this;
     }
 
+    private ExpectedLoggingAssert hasNoMessageWithLevel(ListAssert<LogEvent> logEvents, LogLevel logLevel) {
+        logEvents.withFailMessage("Expected no %s message.\nBut found:\n%s",
+                                  logLevel,
+                                  joinLogEvents(logLevel))
+                 .isEmpty();
+        return this;
+    }
+
     private String joinLogEvents() {
+        return joinLogEvents(actual);
+    }
+
+    private String joinLogEvents(LogLevel logLevel) {
+        return joinLogEvents(filterActual(new LogLevelFilter(logLevel)));
+    }
+
+    private List<? extends LogEvent> filterActual(LogLevelFilter logLevelFilter) {
+        List<? extends LogEvent> result = new ArrayList<>(actual);
+        for (LogEvent logEvent : actual) {
+            if (!logLevelFilter.matches(logEvent)) {
+                result.remove(logEvent);
+            }
+        }
+        return result;
+    }
+
+    private String joinLogEvents(List<? extends LogEvent> actual) {
         if (actual == null || actual.size() <= 0) {
             return "";
         }
